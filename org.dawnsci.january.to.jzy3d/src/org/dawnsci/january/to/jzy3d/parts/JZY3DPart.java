a3493a5dbf6d10a91474e3af4fb0e50d850c66ad
Jonah Graham <jonah@kichwacoders.com>
2018-01-10 11:28:03 +0000

    Add SWT vs AWT and numerous other flags to experiment with different settings

--- org.dawnsci.january.to.jzy3d/src/org/dawnsci/january/to/jzy3d/parts/JZY3DPart.java
+++ org.dawnsci.january.to.jzy3d/src/org/dawnsci/january/to/jzy3d/parts/JZY3DPart.java
@@ -11,7 +11,6 @@ package org.dawnsci.january.to.jzy3d.parts;
 import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -30,47 +29,104 @@ import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.ILazyDataset;
 import org.eclipse.january.dataset.SliceND;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.jzy3d.bridge.swt.Bridge;
 import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.ChartLauncher;
 import org.jzy3d.chart.Settings;
 import org.jzy3d.chart.factories.AWTChartComponentFactory;
+import org.jzy3d.chart.swt.SWTChartComponentFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.colors.ColorMapper;
 import org.jzy3d.colors.colormaps.ColorMapRBG;
+import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.maths.Coord3d;
+import org.jzy3d.maths.Range;
+import org.jzy3d.plot3d.builder.Builder;
+import org.jzy3d.plot3d.builder.Mapper;
+import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
 import org.jzy3d.plot3d.primitives.Point;
 import org.jzy3d.plot3d.primitives.Polygon;
 import org.jzy3d.plot3d.primitives.Shape;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
 
 public class JZY3DPart {
+	/**
+	 * Quality of rendering. See {@link Quality} for options.
+	 */
+	private static final Quality QUALITY = Quality.Intermediate;
+
+	/**
+	 * Turn on or off Hardware Acceleration
+	 */
+	private static final boolean HARDWARE_ACCELERATED = true;
+
+	/**
+	 * Use a diffraction pattern provided by a jpg in this bundle, or if false, use
+	 * a Sin wave.
+	 */
+	private static final boolean USE_DIFFPATTERN = false;
+
+	/**
+	 * Use a SWTChartComponentFactory vs AWTChartComponentFactory as the drawing
+	 * canvas.
+	 */
+	private static final boolean USE_SWT = true;
 
 	private Composite parent;
-
-	private String filename;
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
 		this.parent = parent;
-		parent.setLayout(new GridLayout(1, false));
+		parent.setLayout(new FillLayout());
 
-		// if (filename == null) {
-		// Shell shell = Display.getDefault().getActiveShell();
-		// FileDialog dialog = new FileDialog(shell);
-		// filename = dialog.open();
-		// }
+		Shape surface;
+		if (USE_DIFFPATTERN) {
+			surface = createSurfaceDiffpattern();
+		} else {
+			surface = createSurfaceSin();
+		}
+
+		if (USE_SWT) {
+			drawSurfaceSWTChartComponentFactory(surface, parent);
+		} else {
+			drawSurfaceAWTChartComponentFactory(surface, parent);
+		}
+	}
+
+	private Shape createSurfaceDiffpattern() {
+		Shape surface = null;
 		URL url = FileLocator.find(Activator.getContext().getBundle(), new Path("data/diffpattern2.jpg"), null);
 		try {
 			url = FileLocator.toFileURL(url);
-			filename = URIUtil.toFile(URIUtil.toURI(url)).toString();
-			Shape surface = getSurfaceFromImage(filename);
-			drawSurface(surface, parent);
+			String filename = URIUtil.toFile(URIUtil.toURI(url)).toString();
+			surface = getSurfaceFromImage(filename);
 		} catch (IOException | URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return surface;
+	}
+
+	private Shape createSurfaceSin() {
+		Mapper mapper = new Mapper() {
+			@Override
+			public double f(double x, double y) {
+				return x * Math.sin(x * y);
+			}
+		};
+
+		// Define range and precision for the function to plot
+		Range range = new Range(-3, 3);
+		int steps = 80;
+
+		// Create the object to represent the function over the given range.
+		final Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper);
+		surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(),
+				surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
+		surface.setFaceDisplayed(true);
+		surface.setWireframeDisplayed(false);
+		return surface;
 	}
 
 	/*
@@ -109,28 +165,25 @@ public class JZY3DPart {
 			}
 		}
 		// create a shape with the polygons
-		return new Shape(polygons);
-	}
-
-	/**
-	 * Draw a surface on a JZy3d chart inside of a SWT Composite
-	 *
-	 * @param surface
-	 * @param parent
-	 */
-	private void drawSurface(Shape surface, Composite parent) {
+		Shape surface = new Shape(polygons);
 		surface.setColorMapper(new ColorMapper(new ColorMapRBG(), surface.getBounds().getZmin(),
 				surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
 		surface.setFaceDisplayed(true);
 		surface.setWireframeDisplayed(false);
+		return surface;
+	}
 
-		// Create a chart
-		Chart chart = AWTChartComponentFactory.chart(Quality.Advanced, "awt");
+	private void drawSurfaceSWTChartComponentFactory(Shape surface, Composite parent) {
+		Chart chart = SWTChartComponentFactory.chart(parent, QUALITY);
 		chart.getScene().getGraph().add(surface);
+		Settings.getInstance().setHardwareAccelerated(HARDWARE_ACCELERATED);
+		ChartLauncher.openChart(chart);
+	}
 
-		Settings.getInstance().setHardwareAccelerated(true);
-
-		parent.setLayout(new FillLayout());
+	private void drawSurfaceAWTChartComponentFactory(Shape surface, Composite parent) {
+		Chart chart = AWTChartComponentFactory.chart(QUALITY, "awt");
+		chart.getScene().getGraph().add(surface);
+		Settings.getInstance().setHardwareAccelerated(HARDWARE_ACCELERATED);
 		chart.addKeyboardCameraController();
 		chart.addKeyboardScreenshotController();
 		chart.addMouseCameraController();
